da59478e514f3d77b825f187caf0b489ba90de0c
cobaltdata <nik@cobaltdata.net>
2025-02-08 03:36:31 +0000

    gg

--- frontend/landing/vite.config.ts
+++ frontend/landing/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: "landing", // Set the root directory to landing
-  publicDir: "landing/public", // Ensure Vite serves public assets from here
+  root: "landing", // Set the root directory to the landing folder
+  publicDir: "landing/public", // Ensure Vite finds index.html inside public
   plugins: [react()],
   build: {
     outDir: "dist", // Output the final build to dist/
